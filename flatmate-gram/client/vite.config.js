c0a4d0aa24baaf6ef124bf38616a208469eea090
Richa <richaydav.68211@gmail.com>
2026-03-07 22:56:47 +0530

    Fix Vite allowedHosts for Render2

--- flatmate-gram/client/vite.config.js
+++ flatmate-gram/client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: ['flatmate-frontend-be5x.onrender.com']
-  }
+ server: {
+  allowedHosts: ['.onrender.com']
+}
 })
